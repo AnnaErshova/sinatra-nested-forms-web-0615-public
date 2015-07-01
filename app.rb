0d0a044616c637a3b11282c8abd4dfcdec08830a
AnnaErshova <anna.ershova@gmail.com>
2015-07-01 14:40:59 -0400

    I be done

--- app.rb
+++ app.rb
@@ -3,7 +3,20 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    # index
+    get '/' do
+      erb :'/root'
+    end
 
-  end
-end
+    # new
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = params[:pirate] # this is in leiu of creating a new instance of a Pirate class
+      erb :'/pirates/show'
+    end
+
+  end # end class
+end # end module
